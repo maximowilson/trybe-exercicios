a8ddea93677115f5cb6a006668965e9e6265118f
Wilson Maximo <maximortywil@gmail.com>
2022-10-21 18:44:44 -0300

    Media aritimetica

--- 01-Fundamentos/secao-04-introducao-a-javascript/script.js
+++ 01-Fundamentos/secao-04-introducao-a-javascript/script.js
@@ -1,5 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
 
-for(i = 0; i < numbers.length; i++) {
-    console.log(numbers[i])
+for (i = 0; i < numbers.length; i++) {
+    result += numbers[i];
 }
+let media = result / numbers.length;
+
+console.log(media);
