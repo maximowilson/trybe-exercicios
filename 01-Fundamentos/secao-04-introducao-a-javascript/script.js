1b09d226ac2a028d3f23395bdab0490e41454a5e
Wilson Maximo <maximortywil@gmail.com>
2022-10-21 19:16:05 -0300

    quantos impares tem na lista

--- 01-Fundamentos/secao-04-introducao-a-javascript/script.js
+++ 01-Fundamentos/secao-04-introducao-a-javascript/script.js
@@ -1,9 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let higherNumber = numbers[0];
+let oddNumber = 0;
 
-for (i = 1; i < numbers.length; i++) {
-    if (numbers[i] > higherNumber) {
-        higherNumber = numbers[i];
+for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 !== 0) {
+        oddNumber += 1;
     }
 }
-console.log(higherNumber);
+console.log(oddNumber);
