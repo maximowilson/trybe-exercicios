8dc2b4b0eb2cac2268f00a183e87af158ec08806
Wilson Maximo <maximortywil@gmail.com>
2022-10-25 19:35:06 -0300

    imprimindo frases com objetos

--- 01-Fundamentos/secao-04-introducao-a-javascript/dia-04/script.js
+++ 01-Fundamentos/secao-04-introducao-a-javascript/dia-04/script.js
@@ -1,9 +1,13 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
-
-  info.recorrente = 'Sim';
-
-console.log(info);
+  console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' é ' + leitor.livrosFavoritos[0].titulo + '!');
