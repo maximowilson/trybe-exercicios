fac758834a1e2d8719e119d9299ca644af52341f
Wilson Maximo <maximortywil@gmail.com>
2022-10-25 19:07:50 -0300

    adicionei chave à objeto

--- 01-Fundamentos/secao-04-introducao-a-javascript/dia-04/script.js
+++ 01-Fundamentos/secao-04-introducao-a-javascript/dia-04/script.js
@@ -2,7 +2,8 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim',
   };
 
-console.log('Bem-vinda, ' + info.personagem);
+  info.recorrente = 'Sim';
+
+console.log(info);
