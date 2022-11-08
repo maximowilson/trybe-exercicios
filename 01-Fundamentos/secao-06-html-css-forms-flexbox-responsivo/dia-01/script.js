e873e5545ab3da30b42cd1b69ae97681182efdab
Wilson Maximo <maximortywil@gmail.com>
2022-11-08 19:32:44 -0300

    exercicios dia 6.1 quase concluidos

---
+++ 01-Fundamentos/secao-06-html-css-forms-flexbox-responsivo/dia-01/script.js
@@ -0,0 +1,7 @@
+function handleSubmit(event) {
+    event.preventDefault();
+  }
+window.onload = function () {
+    const apertarBotao = document.querySelector('#btn-reset');
+    apertarBotao.addEventListener('click' handleSubmit);
+    };
