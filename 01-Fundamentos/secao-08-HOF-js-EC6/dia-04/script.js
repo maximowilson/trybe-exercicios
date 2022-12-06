57224556f9d71d06d818ffb312ead73711be751b
Wilson Maximo <maximortywil@gmail.com>
2022-12-06 18:32:10 -0300

    iniciando exercícios dia 8.4

---
+++ 01-Fundamentos/secao-08-HOF-js-EC6/dia-04/script.js
@@ -0,0 +1,17 @@
+const user = {
+    name: 'Maria',
+    age: 21,
+    nationality: 'Brazilian',
+};
+  
+const jobInfos = {
+    profession: 'Software engineer',
+    squad: 'Rocket Landing Logic',
+    squadInitials: 'RLL',
+};
+
+const allInformation = {...user, ...jobInfos};
+
+const {name, age, nationality,profession, squad, squadInitials} = allInformation;
+
+console.log(`'Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}'`);
