e863cee4481d43cd7d6b998399099302d5f79928
Wilson Maximo <maximortywil@gmail.com>
2022-12-06 14:55:29 -0300

    finaliza dia

---
+++ 01-Fundamentos/secao-08-HOF-js-EC6/script.js
@@ -0,0 +1,9 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const multiply = (elemento) => {
+    console.log(elemento * 2);
+}
+
+numbers.forEach(multiply);
+
+// tabuada.forEach((elemento) => console.log(elemento * 2));
